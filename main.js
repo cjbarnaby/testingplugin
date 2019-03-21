67711e4b0e44f769ef42949d04ede42bfaf2cd1a
cjbarnaby <cjbarnaby@gmail.com>
2019-03-22 09:05:50 +1100

    decouple js

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
+
 	console.log("badger");
-	// (function(d, s, id) {
-	// 	var js, fjs = d.getElementsByTagName(s)[0];
-	// 	if (d.getElementById(id)) return;
-	// 	js = d.createElement(s);
-	// 	js.id = id;
-	// 	js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-	// 	fjs.parentNode.insertBefore(js, fjs);
-	// }(document, 'script', 'facebook-jssdk'));
-});
+	window.fbAsyncInit = function() {
+		FB.init({
+			xfbml            : true,
+			version          : 'v3.2'
+		});
+	};
+
+	(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+	fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+
+	document.addEventListener("DOMContentLoaded", function() {
+		console.log(document.querySelector("#num"));
+	});
